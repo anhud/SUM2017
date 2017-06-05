4d76bc5490b332333fd252e698962dfd0adb5c32
Khudyakov Andrey <anhud0000@gmail.com>
2017-06-05 17:13:10 +0300

    cool and new

--- T02CLOCK/T02CLOCK.C
+++ T02CLOCK/T02CLOCK.C
@@ -15,6 +15,7 @@
 #define WND_CLASS_NAME "kazakhstan"
 
 FLOAT pi = 3.1415926535F;
+POINT p[4];
 /* Forward references */
 LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
                                WPARAM wParam, LPARAM lParam );
@@ -66,6 +67,19 @@ INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
   return msg.wParam;
 }
 
+VOID DrawHand(HDC hMemDC, FLOAT t, FLOAT d, FLOAT ang, INT len, INT len2, INT w, INT h)
+{
+  p[0].x = w / 2;
+  p[0].y = h / 2;
+  p[1].x = w / 2 + len2 * cos((t / d) * pi * 2 - pi / 2 - ang);
+  p[1].y = h / 2 + len2 * sin((t / d) * pi * 2 - pi / 2 - ang);
+  p[2].x = w / 2 + len * cos((t / d) * pi * 2 - pi / 2);
+  p[2].y = h / 2 + len * sin((t / d) * pi * 2 - pi / 2);
+  p[3].x = w / 2 + len2 * cos((t / d) * pi * 2 - pi / 2 + ang);
+  p[3].y = h / 2 + len2 * sin((t / d) * pi * 2 - pi / 2 + ang);
+  Polygon(hMemDC, p, 4);
+}
+
 LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
                                WPARAM wParam, LPARAM lParam )
 {
@@ -77,8 +91,12 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
   FLOAT sec, min, hou;
   PAINTSTRUCT ps;
   SYSTEMTIME st;
-  POINT p[4];
   HFONT hFnt, hFntOld;
+  CHAR txt[24] = "fsagdfas", day[4], s[3], m[3], hr[3];
+  RECT rc;
+
+  rc.left = 5;
+  rc.right = w - 5;
 
   switch (Msg)
   {
@@ -133,55 +151,64 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
     min = st.wMinute + sec / 60.0;
     hou = st.wHour + min / 60.0;
 
+    //GetOpenFileName
+    SetDCBrushColor(hMemDC, RGB(50, 50, 50));
+    
+    DrawHand(hMemDC, sec, 60.0, 0.24, 100, 10, w, h);
+    DrawHand(hMemDC, min, 60.0, 0.5, 100, 10, w, h);
+    DrawHand(hMemDC, hou, 12.0, 0.5, 75, 10, w, h);
+
+    switch(st.wDayOfWeek)
+    {
+    case 1:
+      strcpy(day, "Mon"); break;
+    case 2:
+      strcpy(day, "Tue"); break;
+    case 3:
+      strcpy(day, "Wed"); break;
+    case 4:
+      strcpy(day, "Thu"); break;
+    case 5:
+      strcpy(day, "Fri"); break;
+    case 6:
+      strcpy(day, "Sat"); break;
+    case 7:
+      strcpy(day, "Sun"); break;
+    }
+
+    if (st.wSecond < 10)
+      strcpy(s, "0");
+    else
+      strcpy(s, "");
+    if (st.wMinute < 10)
+      strcpy(m, "0");
+    else
+      strcpy(m, "");
+    if (st.wHour < 10)
+      strcpy(hr, "0");
+    else
+      strcpy(hr, "");
+
     /* Output text */
-    hFnt = CreateFont(120, 0, 500, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, PROOF_QUALITY, VARIABLE_PITCH | FF_ROMAN, "Bookman Old Style");
+    hFnt = CreateFont(40, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, PROOF_QUALITY, VARIABLE_PITCH | FF_ROMAN, "TechnicBold");
     hFntOld = SelectObject(hMemDC, hFnt);
     SetTextColor(hMemDC, RGB(5, 5, 5));
-    SetBkMode(hMemDC, TRANSPARENT);                                               
-    TextOut(hMemDC, 100, 300, "honk", 4);
+    SetBkMode(hMemDC, TRANSPARENT);
+    rc.bottom = h / 2 + 280;
+    rc.top = h / 2 + 180;
+    DrawText(hMemDC, txt, sprintf(txt, "%i.%i.%i %s", st.wDay, st.wMonth, st.wYear, day), &rc, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+    rc.bottom = h / 2 + 320;
+    rc.top = h / 2 + 220;
+    DrawText(hMemDC, txt, sprintf(txt, "%i:%i:%s%i", st.wHour, st.wMinute, s, st.wSecond), &rc, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
     SelectObject(hMemDC, hFntOld);
     DeleteObject(hFnt);
 
-    GetOpenFileName
-    SetDCBrushColor(hMemDC, RGB(50, 50, 50));
-    /* Draw second hand */
-    p[0].x = w / 2;
-    p[0].y = h / 2;
-    p[1].x = w / 2 + 10 * cos((sec / 60.0) * pi * 2 - pi / 2 - 0.24);
-    p[1].y = h / 2 + 10 * sin((sec / 60.0) * pi * 2 - pi / 2 - 0.24);
-    p[2].x = w / 2 + 100 * cos((sec / 60.0) * pi * 2 - pi / 2);
-    p[2].y = h / 2 + 100 * sin((sec / 60.0) * pi * 2 - pi / 2);
-    p[3].x = w / 2 + 10 * cos((sec / 60.0) * pi * 2 - pi / 2 + 0.24);
-    p[3].y = h / 2 + 10 * sin((sec / 60.0) * pi * 2 - pi / 2 + 0.24);
-    Polygon(hMemDC, p, 4);
-
-    /* Draw minute hand */
-    p[0].x = w / 2;
-    p[0].y = h / 2;
-    p[1].x = w / 2 + 10 * cos((min / 60.0) * pi * 2 - pi / 2 - 0.5);
-    p[1].y = h / 2 + 10 * sin((min / 60.0) * pi * 2 - pi / 2 - 0.5);
-    p[2].x = w / 2 + 100 * cos((min / 60.0) * pi * 2 - pi / 2);
-    p[2].y = h / 2 + 100 * sin((min / 60.0) * pi * 2 - pi / 2);
-    p[3].x = w / 2 + 10 * cos((min / 60.0) * pi * 2 - pi / 2 + 0.5);
-    p[3].y = h / 2 + 10 * sin((min / 60.0) * pi * 2 - pi / 2 + 0.5);
-    Polygon(hMemDC, p, 4);
-
-    /* Draw hour hand */
-    p[0].x = w / 2;
-    p[0].y = h / 2;
-    p[1].x = w / 2 + 10 * cos((hou / 12.0) * pi * 2 - pi / 2 - 0.5);
-    p[1].y = h / 2 + 10 * sin((hou / 12.0) * pi * 2 - pi / 2 - 0.5);
-    p[2].x = w / 2 + 75 * cos((hou / 12.0) * pi * 2 - pi / 2);
-    p[2].y = h / 2 + 75 * sin((hou / 12.0) * pi * 2 - pi / 2);
-    p[3].x = w / 2 + 10 * cos((hou / 12.0) * pi * 2 - pi / 2 + 0.5);
-    p[3].y = h / 2 + 10 * sin((hou / 12.0) * pi * 2 - pi / 2 + 0.5);
-    Polygon(hMemDC, p, 4);
     return 0;
   case WM_ERASEBKGND:
     return 1;
   case WM_KEYDOWN:
     if (wParam == VK_ESCAPE)
-    {
+    {                                                                                                      
       DestroyWindow(hWnd);
       InvalidateRect(hWnd, NULL, FALSE);
     }
